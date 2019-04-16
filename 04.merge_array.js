7c0e4956354a68f724e2ddeb609566520fcb9200
JZM <somedayjzm@foxmail.com>
2019-04-16 16:27:15 +0800

    debug:测试远程GitHub推送

--- 04.merge_array.js
+++ 04.merge_array.js
@@ -1,3 +1,9 @@
-nums1 = [1,2,3,0,0,0], m = 3
-nums2 = [2,5,6],       n = 3
+// nums1 = [1,2,3,0,0,0], m = 3
+// nums2 = [2,5,6],       n = 3
 
+var nums1 = [1,2,3,0,0,0];
+var nums2 = [2,5,6];
+
+var merge = function(nums1, m, nums2, n){
+  
+};
